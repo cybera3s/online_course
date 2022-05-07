ad77a3c154afe55216462d779b9748893ef1ca0e
ario <cybera.3s@gmail.com>
2022-05-07 16:36:43 +0430

    refactor onlinecourse.urls

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -4,17 +4,14 @@ from django.conf.urls.static import static
 from . import views
 
 app_name = 'onlinecourse'
+
 urlpatterns = [
-    # route is a string contains a URL pattern
-    # view refers to the view function
-    # name the URL
-    path(route='', view=views.CourseListView.as_view(), name='index'),
     path('registration/', views.registration_request, name='registration'),
     path('login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
-    # ex: /onlinecourse/5/
+
+    path(route='', view=views.CourseListView.as_view(), name='index'),
     path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
-    # ex: /enroll/5/
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
     path('submission/<int:course_id>', views.submit, name='submit'),
