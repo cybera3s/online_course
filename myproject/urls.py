81993bf8a31ae2c8e9beb1c484b63a81f6518bc4
ario <cybera.3s@gmail.com>
2023-04-03 21:28:29 +0330

    redirect root req to onlinecourse

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # rediercts every request
+    path('', lambda req: redirect('/onlinecourse/')),
     path('onlinecourse/', include('onlinecourse.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
